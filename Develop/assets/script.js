d81eec5c743398f579062e277390c51c21dc83d0
Sabrina Hanson <sabhanson7@gmail.com>
2022-01-06 21:58:48 -0800

    added comments on html/css. javascript has ability to change box colors depending on the hour of the day. still need to complete local storage when buttons are clicked

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,1 +1,37 @@
-console.log('testing js connection')
+var currentDay = document.querySelector('#currentDay');
+var currentTime = moment().format('HH')
+var scheduleContainer = $('.container')
+currentDay.textContent = moment().format("dddd, MMMM Do");
+
+
+function currentHour () {
+    for (let i = 0; i < 17; i++) {
+        var eachHour = scheduleContainer.children().eq(i).children().eq(1).data('time');
+        if (eachHour < currentTime) {
+            scheduleContainer.children().eq(i).children().eq(1).addClass('past');
+        } else if (eachHour == currentTime) {
+            scheduleContainer.children().eq(i).children().eq(1).addClass('present');
+        } else {
+            scheduleContainer.children().eq(i).children().eq(1).addClass('future')
+        }
+    }
+};
+
+currentHour();
+
+
+//ON PAGE LOAD, ALL STORED INPUT WILL BE DISPLAYED
+//DATE WILL SHOW AT THE TOP
+
+
+    
+//TODO: TIMER FUNCTION THAT CHECKS THE MOMENT TIME EVERY MILLISECOND TO PROVIDE LIVE UPDATES AS THE HOURS PASS
+    
+    
+//TODO: CREATE LOCAL STORAGE TO CONTAIN USER INPUTS IN THE TEXTAREA TAGS.
+    //STORE TEXTAREA INPUT ONCE SAVE BUTTON IS PRESSED
+    //TODO: ON.CLICK ANY SAVEBTN STORAGE IS UPDATED
+
+
+
+
