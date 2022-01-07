bad9bfa03b18560db5e885d5af248f6f4446071c
Sabrina Hanson <sabhanson7@gmail.com>
2022-01-06 23:06:16 -0800

    test

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,3 +1,27 @@
+//CURRENTLY WORKING:
+    //current day is displayed at the top
+    //hour boxes will change from green to red to grey depending on UTC hour
+    //textboxes will receive text and keep it until the page is refreshed
+//NEED TO FIGURE OUT
+    //localStorage for each textbox
+    //clickevent on each save button
+        //can the click event be applied to all buttons so no matter which button the user selects, the information will be saved?
+
+//ACCEPTANCE CRIT
+    // GIVEN I am using a daily planner to create a schedule
+    // WHEN I open the planner
+    // THEN the current day is displayed at the top of the calendar
+    // WHEN I scroll down
+    // THEN I am presented with timeblocks for standard business hours
+    // WHEN I view the timeblocks for that day
+    // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    // WHEN I click into a timeblock
+    // THEN I can enter an event
+    // WHEN I click the save button for that timeblock
+    // THEN the text for that event is saved in local storage
+    // WHEN I refresh the page
+    // THEN the saved events persist
+
 var currentDay = document.querySelector('#currentDay');
 var currentTime = moment().format('HH')
 var scheduleContainer = $('.container')
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # HW5-DayPlanner
 day planner for a user to input appointments, plans, etc.
+
+##Description
+
+I created this schedule planner for a user to be able to input various things on their workday schedule. The site will update daily with the correct day featured in the top section. Additionally, depending on the time of day, the hour boxes will change colors. Grey box means that the hour has already passed. Red box indicates that is the current hour. Green box means that the hour has yet to happen.
+
+##Usage
+
+A user will load the page and click on the specific hour box where they would like to input information. The browser will remember all previously saved information for the user to return to later.
+
+....ADD GIF OF USAGE....
